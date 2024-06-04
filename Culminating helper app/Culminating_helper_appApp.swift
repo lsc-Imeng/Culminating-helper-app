373e51b7dc7db8e5d8cbaa1af93c1c489773a3be
Irving Meng <imeng@lcs.on.ca>
2024-06-04 09:53:24 -0400

    Initial Commit

---
+++ Culminating helper app/Culminating_helper_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Culminating_helper_appApp.swift
+//  Culminating helper app
+//
+//  Created by 孟元森 on 2024-06-04.
+//
+
+import SwiftUI
+
+@main
+struct Culminating_helper_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
